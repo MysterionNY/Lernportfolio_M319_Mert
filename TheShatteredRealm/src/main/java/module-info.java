ae5a30df677478379c43d4de4a2f01d9f0a479b4
MysterionNY <mert.bal@edu.tbz.ch>
2025-01-07 10:55:10 +0100

    Delete TheShatteredRealm directory

--- TheShatteredRealm/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.theshatteredrealm {
-    requires com.google.gson;
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.theshatteredrealm to javafx.fxml;
-    exports com.example.theshatteredrealm;
-}
